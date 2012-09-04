27647428a03a8e69805c04355fc7c909d04341d5
Dan Harrison <hwyfour@gmail.com>
2012-09-03 18:28:07 -0700

    added dynamic room listing, client join/leave alerts, nickname update alert, room additions/deletions, and comments

--- files/front.js
+++ files/front.js
@@ -1,67 +1,199 @@
+/* A client receives:
+ *
+ * connect				A connection confirmation message
+ * roomInfo				The current room that the client belongs to
+ * chatMessage			A regular chat message that we write to the screen
+ * roomAdd				Room details that we add to the room list
+ * roomDel				A room ID that we remove from the room list
+ * nickMessage			An initial message that provides this client with a nickname
+ * typingMessage		An alert that some client is writing a message
+ * stopTypingMessage	An alert that some client stopped writing
+ * clientJoin			An alert that some client joined the room
+ * clientLeave			An alert that some client left the room
+ * otherNickChange		An alert that some client changed its nickname
+ *
+ * A client sends:
+ *
+ * clientURL			The current URL of this client. This is the current room identifier
+ * clientMessage		A chat message sent by this client
+ * clientTyping			An alert that this client is typing
+ * clientNotTyping		An alert that this client stopped typing
+ * clientNick			A message with a new nickname that the server stores for this client
+ *
+ */
+
 var socket = io.connect('http://192.168.1.221');
 var output;
 var userMsg;
 var userNick;
-var currentlyTyping = false;
+var roomPanel;
+var currentRoom;
+var currentlyTyping = false;	//flag to control when a 'currently typing' message is emitted
 
-function writeMsg(data) {
+/* When a client first connects, we send the current URL
+ * back to the server to be used as the current chatroom.
+ * We also update the status icon to show that we are connected.
+ */
+socket.on('connect', function () {
+	socket.emit('clientURL', {data: document.URL});
+	var temp = document.getElementById("status");
+	temp.className = "connected";
+	temp.firstChild.innerHTML = "Connected";
+});
+
+/* When a client sends its URL, it is returned this value which is its current room
+ */
+socket.on('roomInfo', function (data) {
+	currentRoom = data.data;
+	if (currentRoom === undefined) {
+		currentRoom = "";
+	}
+});
+
+/* When a client receives a message, we write it to the chat
+ * panel and then scroll to the very bottom of the page to
+ * keep the conversation focused on this last chat message.
+ */
+socket.on('chatMessage', function (data) {
 	var pre = document.createElement("p");
 	pre.innerHTML = data.data.timeStamp + " " + data.data.nick + ": " + data.data.msg;
 	var red = document.createElement("div");
 	red.className = "message";
-	red.id = data.data.id;
 	red.appendChild(pre);
 	output.appendChild(red);
 
 	//a fix to keep the page scrolled to the bottom
 	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
-}
+});
 
-function writeConnect() {
-	var temp = document.getElementById("status");
-	temp.className = "connected";
-	temp.firstChild.innerHTML = "Connected";
-}
-
-function writeTyping(nick) {
-	var pre = document.createElement("p");
-	pre.innerHTML = nick + " is currently typing.";
-	var red = document.createElement("div");
-	red.className = "message";
-	red.id = nick;
+/* Every time the server sends us an update that says a new
+ * room has been created, this function writes the particulars
+ * to the room list. When a client first connects, this function
+ * will be called numerous times to populate the room list with
+ * all available rooms.
+ */
+socket.on('roomAdd', function (data) {
+	var query = data.data;
+	//if this is the default room, apply the correct styling
+	if (query === "") {
+		var pre = document.createElement("span");
+		pre.innerHTML = "Default";
+		var red = document.createElement("article");
+		var out = document.createElement("a");
+		out.href = "/";
+		out.id = "room-" + query;
+	} else {
+		var pre = document.createElement("span");
+		pre.innerHTML = query.split("%20").join("&nbsp;");
+		var red = document.createElement("article");
+		var out = document.createElement("a");
+		out.href = "?" + query;
+		out.id = "room-" + query;
+	}
+	//if the room query matches the current URL, make this one selected
+	if (query === currentRoom) {
+		red.className = "selected";
+	}
 	red.appendChild(pre);
-	output.appendChild(red);
-
-	//a fix to keep the page scrolled to the bottom
-	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
-}
-
-function stopTyping(nick) {
-	var pre = document.getElementById(nick);
-	output.removeChild(pre);
-}
-
-socket.on('connect', function () {
-	socket.emit('clientURL', {data: document.URL});
-	writeConnect();
+	out.appendChild(red);
+	var last = document.getElementById("addition");
+	roomPanel.insertBefore(out, last);
 });
 
-socket.on('serverMessage', function (data) {
-	writeMsg(data);
-});
-
+/* This function is called when the server responds that the user
+ * has changed their nickname. The function serves as an affirmative
+ * response that the nickname has been changed successfully on the server.
+ */
 socket.on('nickMessage', function (data) {
 	userNick.placeholder = data.data;
 });
 
+/* When a different client starts typing, the server is alerted
+ * which in turn alerts all other clients connected to the same
+ * room. This alert is then written to the chat panel just like
+ * a regular chat message. This function handles the alert we
+ * received from the server.
+ */
 socket.on('typingMessage', function (data) {
-	writeTyping(data.nick);
+	var pre = document.createElement("p");
+	pre.innerHTML = data.nick + " is currently typing.";
+	var red = document.createElement("div");
+	red.className = "message";
+	red.id = data.nick;
+	red.appendChild(pre);
+	output.appendChild(red);
+
+	//a fix to keep the page scrolled to the bottom
+	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
 });
 
+/* Remove the message that shows that a different client is typing.
+ * Generally, this is called when that client erases all of
+ * their input or submits a message to the server. In either
+ * case, there is no more typing happening, so we are alerted to this.
+ */
 socket.on('stopTypingMessage', function (data) {
-	stopTyping(data.nick);
+	var pre = document.getElementById(data.nick);
+	output.removeChild(pre);
 });
 
+/* Remove a room from the list of active rooms. This function is called
+ * when the server alerts us that a room is empty.
+ */
+socket.on('roomDel', function (data) {
+	var pre = document.getElementById("room-" + data.data);
+	roomPanel.removeChild(pre);
+});
+
+/* When a client joins the room, we receive this message.
+ * Just like a 'currently typing' message, we write this to the chat panel.
+ */
+socket.on('clientJoin', function (data) {
+	var pre = document.createElement("p");
+	pre.innerHTML = data.nick + " has joined the room";
+	var red = document.createElement("div");
+	red.className = "message";
+	red.appendChild(pre);
+	output.appendChild(red);
+
+	//a fix to keep the page scrolled to the bottom
+	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
+});
+
+/* When a client leaves the room, we receive this message.
+ * Just like a 'currently typing' message, we write this to the chat panel.
+ */
+socket.on('clientLeave', function (data) {
+	var pre = document.createElement("p");
+	pre.innerHTML = data.nick + " has left the room";
+	var red = document.createElement("div");
+	red.className = "message";
+	red.appendChild(pre);
+	output.appendChild(red);
+
+	//a fix to keep the page scrolled to the bottom
+	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
+});
+
+/* When a client changes their nickname, we receive this message.
+ * Just like a 'currently typing' message, we write this to the chat panel.
+ */
+socket.on('otherNickChange', function (data) {
+	var pre = document.createElement("p");
+	pre.innerHTML = data.old + " is now known as " + data.changed;
+	var red = document.createElement("div");
+	red.className = "message";
+	red.appendChild(pre);
+	output.appendChild(red);
+
+	//a fix to keep the page scrolled to the bottom
+	window.scrollTo(0, document.getElementById("write").clientHeight + 5000);
+});
+
+/* When a user presses enter in the input box, we run this code.
+ * If the input box isn't empty, we send the input to the server as a
+ * message and then clear the input box.
+ */
 function userInput(event) {
 	if (event.keyCode === 13) {
 		//the enter key was pressed, submit the message
@@ -70,12 +202,18 @@ function userInput(event) {
 			socket.emit('clientMessage', {data: msg});
 			//clear the input for re-use
 			userMsg.value = "";
+			//because the message has been sent, the client is no longer typing.
 			currentlyTyping = false;
 			socket.emit('clientNotTyping', {data: 0});
 		}
 	}
 }
 
+/* When a user hits any key in the input box, this function is called.
+ * If the box isn't empty, alert the server that the user is typing.
+ * If the box is empty, the user is not typing anything anymore and
+ * the server is alerted to this.
+ */
 function userTyping(event) {
 	if (userMsg.value !== "") {
 		if (!currentlyTyping) {
@@ -88,6 +226,10 @@ function userTyping(event) {
 	}
 }
 
+/* When a user presses enter in the nickname box, we run this code.
+ * If the box isn't empty, we send te nickname to the server and
+ * lock the nickname box so the user can't change their new nickname.
+ */
 function nickInput(event) {
 	if (event.keyCode === 13) {
 		//the enter key was pressed, submit the message
@@ -104,8 +246,9 @@ window.onload = function () {
 	output = document.getElementById("screen");
 	userMsg = document.getElementById("userMsg");
 	userNick = document.getElementById("userNick");
-	var width = document.getElementById("rooms").clientWidth;
-	document.getElementById('write').style.paddingLeft = width + 40 + "px";
+	roomPanel = document.getElementById("rooms");
+	//push the chat panel to the right so it clears the room list.
+	document.getElementById('write').style.paddingLeft = roomPanel.clientWidth + 40 + "px";
 	userMsg.addEventListener("keydown", userInput, false);
 	userMsg.addEventListener("input", userTyping, false);
 	userNick.addEventListener("keydown", nickInput, false);
--- server.js
+++ server.js
@@ -4,14 +4,17 @@ var nostat		= require('node-static');
 var parser		= require('url');
 var file		= new nostat.Server('./files', { cache: 0 });
 var clients		= {};
+var rooms		= {};
 var messages	= {};
 var msgCount	= 0;
 
 app.listen(80);
 
+rooms["Default"] = "";
+
 function Client() {
 	this.socket = undefined;
-	this.room = "default";
+	this.room = rooms[0];
 	this.nick = "Anon";
 }
 
@@ -37,76 +40,188 @@ function onRequest(request, response) {
 	});
 }
 
-io.sockets.on('connection', function (socket) {
-	//when a client connects, push it onto stack of connected clients
-	var room = "default", client = new Client();
-	client.socket = socket;
-	client.nick += socket.id.substring(0, 7);
-	clients[socket.id] = client;
+/* A server receives:
+ *
+ * clientURL			The current URL of a client. This is the client's current room identifier
+ * clientMessage		A chat message sent by a client
+ * clientTyping			An alert that a client is typing
+ * clientNotTyping		An alert that a client stopped typing
+ * clientNick			A message with a new nickname that the server stores for a client
+ * disconnect			A disconnection confirmation message
+ *
+ * A server sends:
+ *
+ * chatMessage			A chat message that goes out to every client in the current room
+ * otherNickChange		A nickname update the goes out to every client in the current room
+ * typingMessage		An alert to all in the room that this client is writing a message
+ * stopTypingMessage	An alert to all in the room that this client stopped writing a message
+ * nickMessage			A message to a new client with their generated nickname
+ * clientLeave			An alert to all in the room that this client has left the room
+ * clientJoin			An alert to all in the room that this client has joined the room
+ * roomInfo				A message to the connected client with its current room
+ * roomAdd				A message sent to all clients to add a room to their room lists
+ * roomDel				A message sent to all clients to remove a room from their room lists
+ *
+ */
 
+/* When a client connects, they need to be initialized as an object and supplied with
+ * a generated nickname for use in the room.
+ */
+io.sockets.on('connection', function (socket) {
+	var	currentRoom = rooms["Default"],
+		client = new Client(),
+		currentSockID = 0;
+	//when a client connects, push it onto stack of connected clients
+	client.socket = socket;
+	currentSockID = socket.id;
+	client.nick += currentSockID.substring(0, 7);
+	clients[currentSockID] = client;
+	//give this new client a generated nickname
 	socket.emit('nickMessage', {data: client.nick});
 
-	//the initial message from the client with the current URL
+	/* When a client reponds with its current URL, the query string acts as
+	 * the current room for the client. The client is then given an updated list
+	 * of active rooms as well as a history for their current room.
+	 */
 	socket.on('clientURL', function (clientData) {
+		//parse the client's URL query
 		var query = parser.parse(clientData.data).query;
-		if (query !== undefined) {
-			room = query;
+		//return to the client what room they are in
+		socket.emit('roomInfo', {data: query});
+		//write a list of current rooms out to the client
+		for (var x in rooms) {
+			socket.emit('roomAdd', {data: rooms[x]});
 		}
-		client.room = room;
+		//if there is a query...
+		if (query !== undefined) {
+			//and the query is for a nonexistent room
+			if (rooms[query] === undefined) {
+				//add the room to the room list
+				rooms[query] = query;
+				//tell all connected clients to add this new room
+				for (var x in clients) {
+					clients[x].socket.emit('roomAdd', {data: rooms[query]});
+				}
+			}
+			currentRoom = rooms[query];
+		}
+		//update the client object with the current room information
+		client.room = currentRoom;
+
+		//send the client the room history
 		for (var x in messages) {
-			if (messages[x].room === room) {
-				socket.emit('serverMessage', {data: messages[x]});
+			if (messages[x].room === currentRoom) {
+				socket.emit('chatMessage', {data: messages[x]});
+			}
+		}
+
+		//alert all clients in this room that this client has joined
+		for (var x in clients) {
+			if (clients[x].room === currentRoom) {
+				clients[x].socket.emit('clientJoin', {nick: client.nick});
 			}
 		}
 	});
 
-	//the server receives a message from a client
+	/* When a client sends a chat message, we send this message out
+	 * to every client in the current room.
+	 */
 	socket.on('clientMessage', function (clientData) {
-		var msg = new Message();
+		//get the current time for recording purposes
 		var time = new Date();
-		msgCount++;
-		msg.id = msgCount;
-		msg.nick = clients[socket.id].nick;
+		//create the new message object
+		var msg = new Message();
+		//assign this message the next ID
+		msg.id = msgCount++;
+		msg.nick = clients[currentSockID].nick;
 		msg.timeStamp = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
 		msg.msg = clientData.data;
-		msg.room = clients[socket.id].room;
+		msg.room = clients[currentSockID].room;
 		messages[msg.id] = msg;
 		//emit the message back to every client in the same room
 		for (var x in clients) {
-			if (clients[x].room === room) {
-				clients[x].socket.emit('serverMessage', {data: msg});
+			if (clients[x].room === currentRoom) {
+				clients[x].socket.emit('chatMessage', {data: msg});
 			}
 		}
 	});
 
-	//the server receives a typing notification from a client
+	/* When a client starts typing, the server alerts every other user in the room.
+	 */
 	socket.on('clientTyping', function (clientData) {
-		var senderNick = clients[socket.id].nick;
+		var senderNick = clients[currentSockID].nick;
 		//emit the message back to every client in the same room
 		for (var x in clients) {
-			if (clients[x].room === room && clients[x].nick !== senderNick) {
+			if (clients[x].room === currentRoom && clients[x].nick !== senderNick) {
 				clients[x].socket.emit('typingMessage', {nick: senderNick});
 			}
 		}
 	});
 
-	//the server receives a cease typing notification from a client
+	/* When a client stops typing, the server alerts every other user in the room.
+	 */
 	socket.on('clientNotTyping', function (clientData) {
-		var senderNick = clients[socket.id].nick;
+		var senderNick = clients[currentSockID].nick;
 		//emit the message back to every client in the same room
 		for (var x in clients) {
-			if (clients[x].room === room && clients[x].nick !== senderNick) {
+			if (clients[x].room === currentRoom && clients[x].nick !== senderNick) {
 				clients[x].socket.emit('stopTypingMessage', {nick: senderNick});
 			}
 		}
 	});
 
-	//the server receives a nickname from a client
+	/* When a client changes its nickname, the server alerts every user in the room
+	 * to the change. The nickname that the server has stored for the client is also
+	 * updated.
+	 */
 	socket.on('clientNick', function (clientData) {
-		clients[socket.id].nick = clientData.data;
+		//store the old and updated versions of the nickname
+		var old = clients[currentSockID].nick;
+		var update = clientData.data;
+		//alert all clients in this room that this client has changed their nickname
+		for (var x in clients) {
+			if (clients[x].room === currentRoom) {
+				clients[x].socket.emit('otherNickChange', {old: old, changed: update});
+			}
+		}
+		//finally, change the nickname for this connected client
+		clients[currentSockID].nick = update;
 	});
 
+	/* Called when a client disconnects, this function cleans up the serverside.
+	 * First, the server removes the client from the connection list, then alerts
+	 * all other clients in the room that client left. Then, if the room is now empty,
+	 * remove the room from the server and alert all clients to remove it from their
+	 * lists as well.
+	 */
 	socket.on('disconnect', function () {
-		delete clients[socket.id];
+		//get the nickname of the client who is leaving
+		var senderNick = clients[currentSockID].nick;
+		//delete this client from the list of connected clients
+		delete clients[currentSockID];
+		//alert all other clients in that room that this client has left
+		for (var x in clients) {
+			if (clients[x].room === currentRoom) {
+				clients[x].socket.emit('clientLeave', {nick: senderNick});
+			}
+		}
+		//only remove the room if it is not the default room
+		if (currentRoom !== "") {
+			//if the room is now empty, delete it from the list of rooms
+			var inUse = false;
+			for (var x in clients) {
+				if (currentRoom === clients[x].room) {
+					inUse = true;
+				}
+			}
+			if(!inUse) {
+				//remove the room from the server's room collection
+				delete rooms[currentRoom];
+				//alert all clients to remove it from their lists as well
+				for (var x in clients) {
+					clients[x].socket.emit('roomDel', {data: currentRoom});
+				}
+			}
+		}
 	});
 });
